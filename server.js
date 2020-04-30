11e900fee81c49412f70c01276e1c54ec9e7f3ec
ikramsarfraz <m.i.sarfraz@gmail.com>
2020-04-29 21:58:55 -0400

    connection established msgs

--- server.js
+++ server.js
@@ -12,7 +12,16 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Run when client connects
 io.on("connection", (socket) => {
-  console.log("New ws Connection...");
+  // Welcome current user
+  socket.emit("message", "Welcome to ChatCord");
+
+  // Broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // Runs when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
 });
 
 const PORT = process.env.PORT || 3000;
